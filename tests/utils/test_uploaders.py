ed6e9b258a7dc64d107e716c57486744c4fe2845
Aloysius Wong <69571013+aloysiuswwc@users.noreply.github.com>
2022-04-07 20:48:24 +0800

    Updated AWS keys into repository secrets, updated exclude paths

--- tests/utils/test_uploaders.py
+++ tests/utils/test_uploaders.py
@@ -16,8 +16,8 @@ def test_s3_uploader():
     app = create_ctfd()
     with app.app_context():
         app.config["UPLOAD_PROVIDER"] = "s3"
-        app.config["AWS_ACCESS_KEY_ID"] = "AKIAIOSFODNN7EXAMPLE"
-        app.config["AWS_SECRET_ACCESS_KEY"] = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+        app.config["AWS_ACCESS_KEY_ID"] = ${{ secrets.AWS_ACCESS_KEY_ID }}
+        app.config["AWS_SECRET_ACCESS_KEY"] = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
         app.config["AWS_S3_BUCKET"] = "bucket"
 
         uploader = S3Uploader()
@@ -40,8 +40,8 @@ def test_s3_sync():
     app = create_ctfd()
     with app.app_context():
         app.config["UPLOAD_PROVIDER"] = "s3"
-        app.config["AWS_ACCESS_KEY_ID"] = "AKIAIOSFODNN7EXAMPLE"
-        app.config["AWS_SECRET_ACCESS_KEY"] = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+        app.config["AWS_ACCESS_KEY_ID"] = ${{ secrets.AWS_ACCESS_KEY_ID }}
+        app.config["AWS_SECRET_ACCESS_KEY"] = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
         app.config["AWS_S3_BUCKET"] = "bucket"
 
         uploader = S3Uploader()
